e1dd141f17b7a0b0881b1986009f859d0a145a72
Eldainorn <dolencmatej3@gmail.com>
2017-11-28 14:21:45 +0100

    added basic logging

--- src/main/java/ApartmentResource.java
+++ src/main/java/ApartmentResource.java
@@ -5,18 +5,36 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
+import java.util.logging.Logger;
+import com.kumuluz.ee.logs.cdi.Log;
 import org.eclipse.microprofile.metrics.annotation.Metered;
+import javax.inject.Inject;
 
 @ApplicationScoped
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 @Path("/apartments")
+@Log
 public class ApartmentResource {
+
+    private Logger log = Logger.getLogger(ApartmentResource.class.getName());
+
+//    @Inject
+//    private RestProperties restProperties;
+//
+//    @POST
+//    @Path("/healthy")
+//    public Response setHealth(Boolean healthy) {
+//        restProperties.setHealthy(healthy);
+//        log.info("Setting health to " + healthy);
+//        return Response.ok().build();
+//    }
 
     @GET
     @Metered
     public Response getAllApartments() {
         List<Apartment> apartments = Database.getApartments();
+        log.info("Getting all apartments...");
         return Response.ok(apartments).build();
     }
 
